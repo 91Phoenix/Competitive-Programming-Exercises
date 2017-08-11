27fb7ac03ab42820e70d56af541c62f7773d73d9
r.rotella <r.rotella@OPN17304.replynet.prv>
2017-08-11 11:43:51 +0200

    first commit of the exercise

---
+++ test/src/test/Main.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] A = new int[]{-4, 5, 1, -6, 2, 1};
+		
+		Solution s= new Solution();
+		int result =s.solution(A);
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println(result);
+	}
+
+}
---
+++ test/src/test/Solution.java
@@ -0,0 +1,48 @@
+package test;
+
+//you can also use imports, for example:
+//import java.util.*;
+
+//you can write to stdout for debugging purposes, e.g.
+//System.out.println("this is a debug message");
+
+class Solution {
+	
+	public int solution(int[] A) {
+		int inputLength = A.length;
+		if(inputLength==1) return 0;
+		int[] lowerAccumulator = new int[inputLength];
+		int[] higherAccumulator = new int[inputLength];
+		// the next conditon stops before the last two
+		for (int i = 0; i < inputLength; i++) {
+			if (i == 0) {
+				lowerAccumulator[i] = A[i];
+			} else {
+				long result = ((long) lowerAccumulator[i - 1]) + ((long)A[i]);
+				if ((result > Integer.MAX_VALUE || result < Integer.MIN_VALUE)) {
+					return -1;
+				}
+				lowerAccumulator[i] =lowerAccumulator[i - 1]+ A[i];
+			}
+		}
+		for (int i = inputLength - 1; i >= 0; i--) {
+			if (i == inputLength - 1) {
+				higherAccumulator[i] = A[i];
+			} else {
+				long result = ((long) higherAccumulator[i + 1]) + ((long)A[i]);
+				if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+					return -1;
+				}
+				higherAccumulator[i] = higherAccumulator[i + 1]+ A[i];
+			}
+		}
+		for (int i = 0; i < inputLength - 1; i++) {
+			if ((lowerAccumulator[i] - A[i]) == (higherAccumulator[i] - A[i])) {
+				return i;
+			}
+			if (i == inputLength - 2 && (lowerAccumulator[i] == 0))
+				return i;
+		}
+		return -1;
+	}
+}
